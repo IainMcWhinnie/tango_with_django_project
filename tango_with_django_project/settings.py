cc752ea6ff34c52398ba7bf93267ac73bf9849d0
Iain McWhinnie <2566283M@student.gla.ac.uk>
2022-01-28 17:40:18 +0000

    Chapter 4 Pass Test

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processores.media',
+                'django.template.context_processors.media',
             ],
         },
     },
